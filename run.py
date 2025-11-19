fbae73245541ec8c6759d5485c8caeba8ca66658
Juan Camargo <jucamargop@unal.edu.co>
2025-11-19 18:05:44 -0500

    Implemented per2per network

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 from app import create_app
+from app.routes.peers import  peers_bp, connect_to_bootstrap
 
 app = create_app()
+app.register_blueprint(peers_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import sys
+    
+    PORT = int(sys.argv[1])
+    app.config["NODE_PORT"] = PORT
+    
+    bootstrap = None
+
+    if len(sys.argv) >= 3:
+        bootstrap = sys.argv[2]
+
+    with app.app_context():    
+        if bootstrap:
+            connect_to_bootstrap(bootstrap_url=bootstrap)
+        
+    app.run(port=PORT)
