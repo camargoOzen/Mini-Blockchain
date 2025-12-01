b791cd79d3b363bec05b3950ef41cc824124e2a7
Juan Camargo <jucamargop@unal.edu.co>
2025-12-01 03:10:47 -0500

    feat: refactor how flask app startup

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import create_app
+from app.instance import wallets
+from app.services.save_service import load_wallets, load_blockchain, save_blockchain
+from app.models.blockchain import Blockchain
+from app.instance import blockchain
 
 app = create_app()
 
@@ -13,4 +17,16 @@ if __name__ == "__main__":
     app.config["NODE_PORT"] = PORT
     app.config["BOOTSTRAP_PORT"] = bootstrap 
 
-    app.run(port=PORT)
+    loaded_wallets = load_wallets(str(PORT))
+    wallets.update(loaded_wallets)
+
+    loaded_chain = load_blockchain(str(PORT))
+    if loaded_chain:
+        # Assign the loaded chain into the global blockchain instance
+        blockchain.chain = loaded_chain
+        print("Blockchain loaded from disk")
+    else:
+        blockchain = Blockchain()
+        save_blockchain(PORT)
+        
+    app.run(port=PORT, debug=True)
